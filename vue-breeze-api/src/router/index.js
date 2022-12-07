f7a9686c654531cbb9e292ad00c4756e35643c0e
Salim66 <salimhasanriad8@gmail.com>
2022-12-07 14:30:27 +0600

    Install tailwindcss and configure with Login, Register Page Add

--- vue-breeze-api/src/router/index.js
+++ vue-breeze-api/src/router/index.js
@@ -1,11 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
+import Home from '../components/Home.vue';
+
+const routes = [
+  { path: '/', name: "Home", component: Home },
+  { path: '/login', name: "Login", component: () => import('../components/Login.vue') },
+  { path: '/register', name: "Register", component: () => import('../components/Register.vue') },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    
-  ]
+  history: createWebHistory(),
+  routes
 })
 
 export default router
